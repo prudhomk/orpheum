217649df652fdec2be813861ce56d77dbcd6b5ad
Kalan Prudhomme <cleaver9@gmail.com>
2022-09-14 14:48:44 -0700

    removed title card, will adjust later

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -16,10 +16,6 @@ export default function Splash() {
         <img className="logo" src="logo-c.jpeg" alt="logo"></img>
       </header>
       <div>
-        <p>
-          all the world is a stage and we are all players.
-        <br></br>
-          let us be a safe haven for those who perform in the night.</p>
 
        <div class="btn" onClick={handleClick}>
           <span class="btn__text">Enter</span>
