1e2d3ee10de1c7fb2ce757bd6c3b6c4fe65aefb9
Kalan Prudhomme <cleaver9@gmail.com>
2022-09-14 14:48:04 -0700

    redid nav bar, removed bootstrap, planning out pages/site layout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import '../styles/Nav.scss';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
 
-export default function NavHead() {
+
+export default function Nav() {
   return (
-    <Navbar bg='white' className="NavHead">
-      <Container>
-       <Nav.Link href='/home'>Home</Nav.Link>
-       <Nav.Link href='/performers'>Performers</Nav.Link>
-       <Nav.Link href='/top'>Hot 100</Nav.Link>
-       <NavDropdown title="Account" id="basic-nav-dropdown">
-       <NavDropdown.Item href='/user'>Profile</NavDropdown.Item>
-       <NavDropdown.Item href='/favorites'>Favorites</NavDropdown.Item>
-        <NavDropdown.Item href='/settings'>Settings</NavDropdown.Item>
-       </NavDropdown>
-      </Container>
-    </Navbar>
+    <>
+      <div className="Nav">
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#performers">Performers</a></li>
+          <li><a href="#favorites">Favorites</a></li>
+        </ul>
+      </div>
+    </>
   )
 }
+
+
